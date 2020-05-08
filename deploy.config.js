e6474684c5739c4f7af2faa9db9baa1b3eb04bba
yoee <yoeex@outlook.com>
2020-05-09 00:31:08 +0800

    repo

--- deploy.config.js
+++ deploy.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	app: [
     {
-    	name: 'yoee',
+    	name: 'yoeeblog',
     	script: 'server/index.js',
     	env: {
     		COMMON_VARIABLE: 'true'
@@ -17,7 +17,7 @@ module.exports = {
 			user: 'root',
 			host: '47.114.87.244',
 			ref: 'origin/master',
-			repo: 'https://github.com/yoee2Git/yoeeblogreconsition.git',
+			repo: 'https://github.com/yoee2Git/yoeeBlog.git',
 			path: '/var/www/yoee',
 			'pre-deploy': 'git fetch --all',
 			'post-deploy': 'cnpm install && cnpm run build && cnpm start && pm2 startOrRestart deploy.config.js --env production'
