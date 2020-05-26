9396cfaf0e7c7d5cf2dc797d0f33db19548529a7
yoee <yoeex@outlook.com>
2020-05-26 10:12:03 +0800

    Revert "re prot 3000"
    This reverts commit 1a3712d4826aa4de81ee0195c969182143551002.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const bodyParser = require('koa-bodyparser');
 const router = require('koa-router')();
-const cors = require('koa2-cors')
 
 const app = new Koa()
 
@@ -21,14 +20,12 @@ async function start () {
 
   const {
     host = process.env.HOST || '127.0.0.1',
-    port = process.env.PORT || 3000
+    port = process.env.PORT || 9090
   } = nuxt.options.server
 
   app.use(bodyParser({
     extendTypes: ['json', 'from' ,'text']
   }));
-
-  app.use(cors())
 
   app.use(router.routes());   /*启动路由*/
   app.use(router.allowedMethods());
